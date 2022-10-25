11e356210efab0190ad6241898dfaa6e1d0e63f8
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-25 17:37:36 +0530

    String Method

---
+++ StringMethod.java
@@ -0,0 +1,12 @@
+public class StringMethod {
+    public static void main(String[] args) {
+        String name = "Shibu";
+        int i = name.length();
+        if (i == 0) {
+            System.out.println("name is empty");
+        } else {
+            System.out.println("valid name");
+        }
+    }
+
+}
