869faa69b32611719993ddb99df0e509e838ee90
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-25 17:38:05 +0530

    Trim Method.

---
+++ TrimMethod.java
@@ -0,0 +1,8 @@
+public class TrimMethod {
+    public static void main(String[] args) {
+        String name = "  Shibu";
+        System.out.println(name);
+        System.out.println(name.trim());
+    }
+
+}
